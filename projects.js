fe6a971c52d1618f855c78475c5be3ff61f1b58b
marknjoroge <marknnjorogeh@gmail.com>
2023-04-14 04:05:23 +0300

    first commit

---
+++ projects.js
@@ -0,0 +1,51 @@
+export var data = [{
+	"type": "website",
+	"tech": ["react js", "node js"],
+	"name": "Elizabeth Wangari & Co Advocates",
+	"link": "https://marknjoroge.github.io/Elizabeth-Law-Firm/",
+	"description": "A minimalistic website for a law firm. The website is built using React JS and Bootstrap 4.",
+	"images": [
+		"elizabeth_wangari/1.png",
+		"elizabeth_wangari/2.png",
+		"elizabeth_wangari/3.png",
+		"elizabeth_wangari/4.png",
+	],
+}, {
+	"type": "website",
+	"tech": ["react js", "node js", "express js", "mysql"],
+	"name": "Accounts Escrow",
+	"link": "https://accountsescrow.co.ke/",
+	"description": "A website for a company that provides escrow services.",
+	"images": [
+		"accounts_escrow/1.png",
+		"accounts_escrow/2.png",
+	],
+}, {
+	"type": "app",
+	"tech": ["java"],
+	"name": "NutriPro",
+	"link": "",
+	"description": "A mobile application that helps users to track their daily calorie intake by scanning te food they are taking.",
+	"images": [
+	],
+}, {
+	"type": "app",
+	"tech": ["flutter"],
+	"name": "Hospital app UI hackathon",
+	"link": "https://twitter.com/gdsc_moi/status/1347580310528159744?s=20",
+	"description": "A university UI hackathon project for a hospital app.",
+	"images": [
+		"hospital_app/1.jpg",
+		"hospital_app/2.jpg",
+		"hospital_app/3.jpg",
+	],
+}, {
+	"type": "app",
+	"tech": ["flutter"],
+	"name": "Music app UI hackathon",
+	"link": "https://twitter.com/gdsc_moi/status/1355133750179319810?s=20",
+	"description": "A university UI hackathon project for a music app.",
+	"images": [
+		"music_app/1.png",
+	],
+}]
